6515ba9e97197c04f35b38c78bc2644064c4727e
wangym <wangym0203@gmail.com>
2024-04-25 17:08:58 +0800

    feat: complete logic (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-/**
- * - 发布插件，解决编译报错，watch
- * - 在input中回显 apikey
- * - 存储翻译结果
- * - UI 优化
- * - 接口请求错误提示
- */
 import { FigmaStorage, StorageKey } from "./FigmaStorage";
 import { Translator } from "./Translator";
 import { MessageData, MessageType } from "./typing";
@@ -12,6 +5,69 @@ import { MessageData, MessageType } from "./typing";
 (async function () {
   const figmaStorage = new FigmaStorage();
   const translator = new Translator();
+  const translateCacheMap = new Map<string, string>();
+
+  async function translate() {
+    const selectedNodes = figma.currentPage.selection;
+    const currentNode = selectedNodes[0] as
+      | {
+          text: TextSublayerNode;
+        }
+      | undefined;
+
+    if (currentNode === undefined) {
+      return;
+    }
+    
+    let characters: string | undefined;
+    if (
+      (
+        currentNode as {
+          text: TextSublayerNode;
+        }
+      ).text
+    ) {
+      characters = currentNode.text.characters;
+    }
+
+    if ((currentNode as unknown as TextSublayerNode).characters) {
+      characters = (currentNode as unknown as TextSublayerNode).characters;
+    }
+
+    if (characters) {
+      if (translateCacheMap.has(characters)) {
+        return figma.ui.postMessage(
+          new MessageData(MessageType.translate, {
+            value: translateCacheMap.get(characters)!,
+            data: { characters },
+          })
+        );
+      }
+
+      figma.ui.postMessage(
+        new MessageData(MessageType.translate, {
+          value: "Loading...",
+          data: { characters },
+        })
+      );
+
+      try {
+        const result = await translator.translate(characters);
+
+        const value = result.choices[0].message.content;
+        figma.ui.postMessage(
+          new MessageData(MessageType.translate, {
+            value: value,
+            data: { characters },
+          })
+        );
+        translateCacheMap.set(characters, value);
+      } catch (error) {
+        console.error(error);
+        figma.notify("fetch api error");
+      }
+    }
+  }
 
   if (figma.editorType === "figma") {
     // TODO add logic
@@ -20,44 +76,22 @@ import { MessageData, MessageType } from "./typing";
 
   if (figma.editorType === "figjam") {
     const APIkey = await figmaStorage.get(StorageKey.APIkey);
-   
+
     figma.showUI(__html__);
 
     if (APIkey) {
-      console.log(`Get the open-api key: ${APIkey}`);
+      // console.log(`Get the open-api key: ${APIkey}`);
       translator.setAPIkey(APIkey);
-
+      figma.ui.postMessage(
+        new MessageData(MessageType.APIkey, {
+          value: APIkey,
+        })
+      );
+      translate();
     }
 
-
     figma.on("selectionchange", async () => {
-      const selectedNodes = figma.currentPage.selection;
-
-      console.log(selectedNodes, "selectedNodes");
-
-      const currentNode = selectedNodes[0] as {
-        text: TextSublayerNode;
-      };
-
-      if (currentNode.text) {
-        const characters = currentNode.text.characters;
-
-        figma.ui.postMessage(
-          new MessageData(MessageType.translate, {
-            value: "Loading...",
-            data: { characters },
-          })
-        );
-
-        const result = await translator.translate(characters);
-
-        figma.ui.postMessage(
-          new MessageData(MessageType.translate, {
-            value: result.choices[0].message.content,
-            data: { characters },
-          })
-        );
-      }
+      await translate();
     });
 
     figma.ui.onmessage = async (msg: MessageData) => {
